3cb73aedddb039cb9ea8214374d276beda51e80f
student <student@student23.local>
2019-08-09 17:27:23 -0300

    whatever

--- iAssist/Assistencia.swift
+++ iAssist/Assistencia.swift
@@ -121,12 +121,21 @@ class Assistencia {
 
 
 class AssistenciaMock {
+    
+    var arrayAssistencia = [Assistencia]()
+    
+    func addAssistencia(_ titulo: String,_ descricao: String,_ historia: String){
+        
+        //arrayAssistencia.append()
+    }
+    
     static func mockAssistencias() -> [Assistencia] {
         return [
             Assistencia(titulo: "Familia no Itamaraty necessita advogado", descricao: "Solicitacao de Assistencia Juridica", historia: "A Empregada domestica Juruminha de Souza foi demitida apos sofrer um acidente de trabalho e necessita de assistencia Juridica"),
             Assistencia(titulo: "Mae no Caioba necessita leite NAN", descricao: "Solicitacao de Doacao", historia: "Apos perder o emprego a dona de casa Claudete Pereira ficou sem condicoes de comprar leite para o seu recenm nascido Felipe.", donation: Donation(items: ["Leite NAN"])),
             Assistencia(titulo: "Familia teve casa incendiada ", descricao: "Solicitacao de Abrigo", historia: "Apos deixar velas acesar por ter a fiacao da casa roubada, a casa foi incendiada num acidente com as velas", imagem: "assistenciaAbrigoCasaIncendiada"),
             Assistencia(titulo: "Crianca necessita de materiais escolares", descricao: "Solicitacao de Valores", historia: "Apos ficar sem emprego , seu Jao nao tem dinheiro para comprar o material de seus 3 filhos", imagem: "assistenciaValorMaterialEscolar", donation: Donation(value: 50.00))
+            
             
         ]
     }
